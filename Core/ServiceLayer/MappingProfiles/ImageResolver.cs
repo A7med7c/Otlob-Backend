80d331cbebeaa9cfb28f152bcfac10b648c4d3bd
Ahmed Ragab <ragabali@std.mans.edu.eg>
2026-04-01 01:25:30 +0200

    Implement Resource Resolver

---
+++ Core/ServiceLayer/MappingProfiles/ImageResolver.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using DomainLayer.Models;
+using Microsoft.Extensions.Configuration;
+using Shared.DTOs;
+
+namespace ServiceImplementation.MappingProfiles;
+
+public class ImageResolver(IConfiguration _configuration) : IValueResolver<Product, ProductDto, string>
+{
+    public string Resolve(Product source, ProductDto destination, string destMember, ResolutionContext context)
+    {
+        if (string.IsNullOrEmpty(source.PictureUrl))
+            return string.Empty;
+        var url = $"{_configuration.GetSection("Urls")["BaseUrl"]}{source.PictureUrl}";
+        return url;
+    }
+}
---
+++ Core/ServiceLayer/MappingProfiles/ProductProfile.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using DomainLayer.Models;
+using Shared.DTOs;
+
+namespace ServiceImplementation.MappingProfiles;
+
+public class ProductProfile : Profile
+{
+    public ProductProfile()
+    {
+        CreateMap<Product, ProductDto>()
+            .ForMember(dist => dist.BrandName, options => options.MapFrom(src => src.ProductBrand.Name))
+            .ForMember(dist => dist.TypeName, options => options.MapFrom(src => src.ProductType.Name))
+            .ForMember(dist => dist.ImageUrl, options => options.MapFrom<ImageResolver>());
+
+        CreateMap<ProductType, TypeDto>();
+        CreateMap<ProductBrand, BrandDto>();
+    }
+}
