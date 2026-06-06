edc636629cc542133148d6d1451889cc9fa6c2f9
Ahmed Ragab <ragabali@std.mans.edu.eg>
2026-06-06 16:10:33 +0300

    Add caching functionality with ICashRepository and CashService implementation

---
+++ Infrastructure/Persistence/Repositories/CashRepository.cs
@@ -0,0 +1,18 @@
+﻿using StackExchange.Redis;
+
+namespace Persistence.Repositories;
+
+internal class CashRepository(IConnectionMultiplexer connection) : ICashRepository
+{
+    readonly IDatabase database = connection.GetDatabase();
+    public async Task<string?> GetAsync(string key)
+    {
+        var value = await database.StringGetAsync(key);
+        return value.IsNullOrEmpty ? null : value.ToString();
+    }
+
+    public async Task SetAsync(string key, string value, TimeSpan lifeTime)
+    {
+        await database.StringSetAsync(key, value, lifeTime);
+    }
+}
